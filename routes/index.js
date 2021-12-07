5d421a85ad89eb65273e99d66850992b2d712738
inchara_nagaraju <incharanagaraju05@gmail.com>
2021-12-07 04:34:51 -0500

    added routes for index

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const DoctorRoutes = require("./doctors");
 const DiseaseRoutes = require("./disease");
+const routes = require('./users');
 
 
 const constructorMethod = (app) => {
   app.use("/doctors", DoctorRoutes);
   app.use("/disease", DiseaseRoutes);
- 
+  app.use('/', routes);
   app.use("*", (req, res) => {
     res.status(404).json({ error: "Not found" });
   });

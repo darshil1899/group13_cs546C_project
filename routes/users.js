22c8e3836a2696e8cfc2adfd786bc9fe5a20fd63
inchara_nagaraju <incharanagaraju05@gmail.com>
2021-12-07 01:02:40 -0500

    added routes for patient

--- routes/users.js
+++ routes/users.js
@@ -1,41 +1,66 @@
-const {ObjectId} = require('bson');
+const { ObjectId } = require('bson');
 const express = require("express");
 const { users } = require("../config/mongoCollections");
-const router= express.Router();
-const data= require('../data/users');
+const router = express.Router();
+const data = require('../data/users');
 
-router.get('/', async(req,res) =>{
+router.get('/', async (req, res) => {
     let reqSes = Object.keys(req.session);
-    if(reqSes.includes('username')){
+    if (reqSes.includes('username')) {
         res.redirect("/patient");
         return;
     }
-    else{
+    else {
         res.render("login/login");
         return;
     }
 });
 
-router.post('/login', async(req,res) => {
-    try{
+router.post('/login', async (req, res) => {
+    try {
         if (!req.body.username || !req.body.password) {
-            res.status(400).render("login/login", { error: "Username or Password is not provided "});
+            res.status(400).render("login/login", { error: "Username or Password is not provided " });
             return;
         }
         const User = await data.checkUser(req.body.username, req.body.password);
-        // console.log(User); 
-        if(User.authenticated){
-        // { authenticated: true, authenticatedUser: user }
-        req.session.username = req.body.username.toLowerCase();
- 
-        // req.session.userId = User.authenticatedUser._id.toString();
+
+        if (User.authenticated) {
+
+            req.session.username = req.body.username.toLowerCase();
+
             res.redirect("/patient");
             return;
         }
     }
-    catch(e){
-       console.log(e)
-        res.status(400).render("login/login",{error:"Username and password is not valid"});
+    catch (e) {
+        console.log(e)
+        res.status(400).render("login/login", { error: "Username and password is not valid" });
         return;
     }
+});
+
+router.get('/patient', async (req, res) => {
+    try {
+        let sess = req.session;
+
+        res.render("patient/patient", { username: sess['username'] });
+        return;
+
+    }
+    catch (e) {
+        console.log(e);
+    }
+});
+
+router.get('/book', async (req, res) => {
+
+    res.render("book/book", { username: (req.session)['username'] });
+    return;
+});
+
+router.get('/profile', async (req, res) => {
+
+    let sess = req.session;
+    res.render("profile/profile", { username: sess['username'], firstname: sess['firstname'] });
+    return;
 });

58868b39f48a12a51dff05b62f5ff16a1a8754cb
Ar Koushal <arkoushal1999@gmail.com>
2021-12-13 01:44:34 -0500

    updated handlebars and data part

--- data/users.js
+++ data/users.js
@@ -66,11 +66,11 @@ module.exports = {
     const userCollection = await users();
     const appointmentCollection = await appointments();
     const l_name = username.toLowerCase();
+    console.log(password);
     const ha_pswrd = await bcryptjs.hash(password, saltRounds);
     let newUser = {
       firstname: firstname,
       lastname: lastname,
-      // city: city,
       dateofbirth: dateofbirth,
       username: l_name,
       password: ha_pswrd,
@@ -133,19 +133,16 @@ module.exports = {
 
     const userCollection = await users();
     let name = await userCollection.findOne({ username: username });
-    console.log(name);
     let compare = false;
-    try {
-      compare = await bcryptjs.compare(password, name.password);
-    } catch (e) {
-      console.log(e);
-    }
+    console.log(password, name.password);
+    compare = await bcryptjs.compare(password, name.password);
+    console.log(compare);
     if (compare) return { authenticated: true, loggedinuser: name };
     else {
       throw "Invaild password or username";
     }
   },
-  async updateProfile(id, firstname, lastname, gender, email, phonenumber) {
+  async updateProfile(id, firstname, lastname, gender, phonenumber) {
     // if(!(ObjectId.isValid(id)))
     //     throw 'You must provide a valid id'
     // if (username.length === 0 || lastname.length==0 ||dateofbirth.length==0 || city.length==0 || phonenumber.length==0 ||email.length==0 || gender.length==0)
@@ -181,15 +178,13 @@ module.exports = {
     // }
 
     const proInfo = await this.get(id);
-    // let proLower = users.toLowerCase();
     let proUpdate = {
       firstname: firstname,
       lastname: lastname,
       gender: gender,
-      email: email,
       phonenumber: phonenumber,
     };
-    console.log(proUpdate);
+
     const userCollection = await users();
     const proUpdateInfo = await userCollection.updateOne(
       { _id: new ObjectId(id) },
@@ -207,7 +202,6 @@ module.exports = {
     gender,
     email,
     phonenumber,
-    problem,
     doctor,
     date,
     time
@@ -215,13 +209,12 @@ module.exports = {
     if (
       typeof firstname !== "string" ||
       typeof lastname !== "string" ||
-      typeof dateofbirth !== "date" ||
+      typeof dateofbirth !== "string" ||
       typeof gender !== "string" ||
-      typeof email !== "email" ||
-      typeof phonenumber !== "number" ||
-      typeof problem !== "string" ||
+      typeof email !== "string" ||
+      typeof phonenumber !== "string" ||
       typeof doctor !== "string" ||
-      typeof date !== "date" ||
+      typeof date !== "string" ||
       typeof time !== "string"
     ) {
       throw "Username should be a string";
@@ -233,7 +226,6 @@ module.exports = {
       !email ||
       !phonenumber ||
       !gender ||
-      !problem ||
       !doctor ||
       !date ||
       !time
@@ -244,56 +236,55 @@ module.exports = {
       lastname.length === 0 ||
       email.length === 0 ||
       phonenumber.length === 0 ||
-      problem.length === 0 ||
       date.length === 0 ||
       doctor.length === 0 ||
       dateofbirth.length === 0
     ) {
       throw "Username and password cannot be a empty string";
     }
-    if (firstname.trim().length === 0) {
-      throw "firstname cannot have empty spaces";
-    }
-    if (/\s/.test(firstname)) {
-      throw "firstname cannot have white spaces";
-    }
-    if (lastname.trim().length === 0) {
-      throw "lastname cannot have empty spaces";
-    }
-    if (/\s/.test(lastname)) {
-      throw "lastname cannot have white spaces";
-    }
-    if (email.trim().length === 0) {
-      throw "email cannot have empty spaces";
-    }
-    if (/\s/.test(email)) {
-      throw "email cannot have white spaces";
-    }
-    if (phonenumber.trim().length === 0) {
-      throw "phonenumber cannot have empty spaces";
-    }
-    if (/\s/.test(phonenumber)) {
-      throw "phonenumber cannot have white spaces";
-    }
-    if (firstname.length < 4) {
-      throw "firstname should contain at least 4 characters";
-    }
-    if (lastname.length < 4) {
-      throw "lastname should contain at least 4 characters";
-    }
-    if (phonenumber.length < 10) {
-      throw "password should contain  10 characters";
-    }
-    let ptn = "^[a-zA-Z0-9]*$";
-    if (username.match(ptn) === null) {
-      throw "username should contain only alphanumeric characters";
-    }
+    // if (firstname.trim().length === 0) {
+    //   throw "firstname cannot have empty spaces";
+    // }
+    // if (/\s/.test(firstname)) {
+    //   throw "firstname cannot have white spaces";
+    // }
+    // if (lastname.trim().length === 0) {
+    //   throw "lastname cannot have empty spaces";
+    // }
+    // if (/\s/.test(lastname)) {
+    //   throw "lastname cannot have white spaces";
+    // }
+    // if (email.trim().length === 0) {
+    //   throw "email cannot have empty spaces";
+    // }
+    // if (/\s/.test(email)) {
+    //   throw "email cannot have white spaces";
+    // }
+    // if (phonenumber.trim().length === 0) {
+    //   throw "phonenumber cannot have empty spaces";
+    // }
+    // if (/\s/.test(phonenumber)) {
+    //   throw "phonenumber cannot have white spaces";
+    // }
+    // if (firstname.length < 4) {
+    //   throw "firstname should contain at least 4 characters";
+    // }
+    // if (lastname.length < 4) {
+    //   throw "lastname should contain at least 4 characters";
+    // }
+    // if (phonenumber.length < 10) {
+    //   throw "password should contain  10 characters";
+    // }
+    // // let ptn = "^[a-zA-Z0-9]*$";
+    // // if (username.match(ptn) === null) {
+    // //   throw "username should contain only alphanumeric characters";
+    // // }
 
-    var validRegex =
-      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    if (!email.match(validRegex)) {
-      throw "Put a valid mail id ";
-    }
+    // var validRegex =
+    //   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    // if (!email.match(validRegex)) {
+    //   throw "Put a valid mail id ";
+    // }
 
     const appointmentCollection = await appointments();
     let newUser = {
@@ -303,19 +294,47 @@ module.exports = {
       gender: gender,
       email: email,
       phonenumber: phonenumber,
-      problem: problem,
+      problem: doctor,
       date: date,
       time: time,
+      comments: [],
     };
 
-    // let appointmentdata=await appointmentCollection.findOne({_id: });
-    // if(present !== null)
-    //     throw "This username is already present"
     let insertInfo = await appointmentCollection.insertOne(newUser);
     let aid = insertInfo.insertedId;
-    console.log(insertInfo);
+
     let appointmentdata = await appointmentCollection.findOne({ _id: aid });
-    // throw"User is not created"
     return appointmentdata;
+  },
+
+  async getAppointmentByEmailAddress(emailAddress) {
+    const appointmentCollection = await appointments();
+    const allMyAppointments = await appointmentCollection
+      .find({ email: emailAddress })
+      .toArray();
+    return allMyAppointments;
+  },
+
+  async addAppointmentComment(appointmentId, comment, flag) {
+    const commenter = flag === "doctor" ? true : false;
+    const appointmentCollection = await appointments();
+    const userToInsert = {
+      byDoctor: commenter,
+      comment: comment,
+    };
+    const addComment = await appointmentCollection.updateOne(
+      {
+        _id: new ObjectId(appointmentId),
+      },
+      {
+        $addToSet: { comments: userToInsert },
+      }
+    );
+    return true;
+  },
+
+  async getAllAppointments() {
+    const appointmentCollection = await appointments();
+    return await appointmentCollection.find({}).toArray();
   },
 };
